abce63f83fcd0755b3758e5f529578470fdc3b7a
Matthew Clewell <matt@mattclewell.com>
2023-02-19 17:46:53 -0600

    lemp9: add nixos-hardware flakes

--- flake.nix
+++ flake.nix
@@ -13,12 +13,7 @@
     nur.url = "github:nix-community/NUR";
     #nur.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
+    hardware.url = "github:nixos/nixos-hardware";
   };
 
   outputs = { self, nixpkgs, home-manager, nur, ... }@inputs: 
--- hosts/lemp9/default.nix
+++ hosts/lemp9/default.nix
@@ -1,6 +1,10 @@
 { pkgs, inputs, ... }:
 {
   imports = [
+    inputs.hardware.nixosModules.common-cpu-intel
+    inputs.hardware.nixosModules.common-gpu-intel
+    inputs.hardware.nixosModules.common-pc-ssd
+    inputs.hardware.nixosModules.system76
     ./hardware-configuration.nix
     ../common
     ../common/de/gnome.nix
